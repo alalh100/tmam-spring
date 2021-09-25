10817988950b023e6befc3ac957697cebcd05165
alalh100 <alalh100@hhu.de>
2021-09-25 10:01:59 +0200

    first commit

---
+++ src/main/java/com/tmam/springtmam/SpringTmamApplication.java
@@ -0,0 +1,13 @@
+package com.tmam.springtmam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTmamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTmamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tmam/springtmam/SpringTmamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmam.springtmam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTmamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
